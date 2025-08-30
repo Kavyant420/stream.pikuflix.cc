390540d73715d92d90c3ae040fd7ca4dbccb3486
pikuflix <kavyantverma62@gmail.com>
2025-08-30 10:40:12 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -11,9 +11,16 @@ export default async function handler(req, res) {
     const response = await fetch(targetUrl, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115 Safari/537.36"
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117 Safari/537.36",
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://vidsrc.cc/",
+        "Cache-Control": "no-cache",
+        "Pragma": "no-cache",
       }
     });
+
     const data = await response.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
